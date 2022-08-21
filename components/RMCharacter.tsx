3497231741b56b0bc90c0cf18babbb397b9c319a
Robério <roberiomatos@yahoo.com.br>
2022-08-20 23:20:35 -0300

    updated styles

--- components/RMCharacter.tsx
+++ components/RMCharacter.tsx
@@ -67,7 +67,7 @@ const RMCharacter = () => {
                             style={styles.closeButton}
                             onPress={() => setModalVisible(false)}
                         >
-                            <Text style={{ fontSize: 15, color: '#b6b6b6' }}>Return</Text>
+                            <Text style={{ fontSize: 15, color: '#939393' }}>Return</Text>
                         </Pressable>
 
                     </View>
